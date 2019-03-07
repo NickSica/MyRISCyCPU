208b8554563024b80ac1d0dad6b7fcdc233a52fa
Sica <Nick.Sica@ARRIS.COM>
2019-03-07 15:25:16 -0500

    Changes

--- alu.sv
+++ alu.sv
@@ -1,3 +1,62 @@
-module alu(input logic rd, input logic op1, input logic op2);
-
+module alu
+    (input logic[3:0] operation, 
+    input unsigned logic[31:0] op1, 
+    input unsigned logic[31:0] op2, 
+    output logic[31:0] op_out);
+    
+    case(instruction)
+        LUI     : 
+        AUIPC   :
+        JAL     :
+        JALR    :
+        //BRANCH Instructions
+        BEQ     :
+        BNE     :
+        BLT     :
+        BGE     :
+        BLTU    :
+        BGEU    :
+        //LOAD Instructions
+        LB      :
+        LH      :
+        LW      :
+        LBU     :
+        LHU     :
+        //STORE Instructions
+        SB      : 
+        SH      : 
+        SW      : 
+        //OP-IMM Instructions
+        ADDI    : 
+        SLTI    : 
+        SLTIU   : 
+        XORI    : 
+        ORI     : 
+        ANDI    : 
+        SLLI    : 
+        SRLI    : 
+        SRAI    : 
+        //OP Instructions
+        ADD     : assign op_out = 32'(op1 + op2);
+        SUB, SUBI: assign op_out = 32'(op1 - op2);
+        SLL     : assign op_out = op1 << op2; 
+        SLT     : assign op_out = (signed'(op1) < signed'(op2) ? 32'b1 : 32'b0);
+        SLTU    : assign op_out = (op1 < op2 ? 32'b1 : 32'b0);
+        XOR     : assign op_out = op1 ^ op2;
+        SRL     : assign op_out = op1 >> op2;
+        SRA     : assign op_out = op1 >>> op2;
+        OR      : assign op_out = op1 | op2;
+        AND     : assign op_out = op1 & op2;
+        //MISC-MEM Instructions
+        FENCE   :
+        FENCE_I :
+        //SYSTEM Instructions
+        ECALL   :
+        EBREAK  :
+        CSRRW   :
+        CSRRS   :
+        CSRRC   :
+        CSRRWI  :
+        CSRRSI  :
+        CSRRCI  :
 endmodule: alu
--- design.sv
+++ design.sv
@@ -6,3 +6,67 @@ interface registers(input logic clk);
     logic [31:0] x30, x31, x32;
     logic [31:0] pc;
 endinterface: registers
+
+module top(input logic[31:0] encoded_value);
+    instructions::instructions instruction;
+
+    instructions::decode decoder0(.encoded_value(encoded_value), .instruction(instruction));
+    case(instruction)
+        LUI     : 
+        AUIPC   :
+        JAL     :
+        JALR    :
+        //BRANCH Instructions
+        BEQ     :
+        BNE     :
+        BLT     :
+        BGE     :
+        BLTU    :
+        BGEU    :
+        //LOAD Instructions
+        LB      :
+        LH      :
+        LW      :
+        LBU     :
+        LHU     :
+        //STORE Instructions
+        SB      : 
+        SH      : 
+        SW      : 
+        //OP-IMM Instructions
+        ADDI    : 
+        SUBI    :
+            alu alu0(.operation(4'b0001), .op1(), .op2(), .op_out());
+        SLTI    : 
+        SLTIU   : 
+        XORI    : 
+        ORI     : 
+        ANDI    : 
+        SLLI    : 
+        SRLI    : 
+        SRAI    : 
+        //OP Instructions
+        ADD: alu alu0(.operation(4'b0000), .op1(), .op2(), .op_out());
+        SUB: alu alu0(.operation(4'b0001), .op1(), .op2(), .op_out());
+        SLL: op_out = op1 << op2; 
+        SLT: op_out = (signed'(op1) < signed'(op2) ? 32'b1 : 32'b0);
+        SLTU: op_out = (op1 < op2 ? 32'b1 : 32'b0);
+        XOR: op_out = op1 ^ op2;
+        SRL: op_out = op1 >> op2;
+        SRA: op_out = op1 >>> op2;
+        OR: op_out = op1 | op2;
+        AND: op_out = op1 & op2;
+        //MISC-MEM Instructions
+        FENCE   :
+        FENCE_I :
+        //SYSTEM Instructions
+        ECALL   :
+        EBREAK  :
+        CSRRW   :
+        CSRRS   :
+        CSRRC   :
+        CSRRWI  :
+        CSRRSI  :
+        CSRRCI  :
+        default: op_out = 'z
+endmodule: top
