4c183aebc010ece898fb1aeca7bc26859b022d22
Sica <Nick.Sica@ARRIS.COM>
2019-03-07 18:03:55 -0500

    Changes

--- alu.sv
+++ alu.sv
@@ -1,62 +1,64 @@
-module alu
+module Alu
     (input logic[3:0] operation, 
     input unsigned logic[31:0] op1, 
     input unsigned logic[31:0] op2, 
     output logic[31:0] op_out);
     
-    case(instruction)
-        LUI     : 
-        AUIPC   :
-        JAL     :
-        JALR    :
-        //BRANCH Instructions
-        BEQ     :
-        BNE     :
-        BLT     :
-        BGE     :
-        BLTU    :
-        BGEU    :
-        //LOAD Instructions
-        LB      :
-        LH      :
-        LW      :
-        LBU     :
-        LHU     :
-        //STORE Instructions
-        SB      : 
-        SH      : 
-        SW      : 
-        //OP-IMM Instructions
-        ADDI    : 
-        SLTI    : 
-        SLTIU   : 
-        XORI    : 
-        ORI     : 
-        ANDI    : 
-        SLLI    : 
-        SRLI    : 
-        SRAI    : 
-        //OP Instructions
-        ADD     : assign op_out = 32'(op1 + op2);
-        SUB, SUBI: assign op_out = 32'(op1 - op2);
-        SLL     : assign op_out = op1 << op2; 
-        SLT     : assign op_out = (signed'(op1) < signed'(op2) ? 32'b1 : 32'b0);
-        SLTU    : assign op_out = (op1 < op2 ? 32'b1 : 32'b0);
-        XOR     : assign op_out = op1 ^ op2;
-        SRL     : assign op_out = op1 >> op2;
-        SRA     : assign op_out = op1 >>> op2;
-        OR      : assign op_out = op1 | op2;
-        AND     : assign op_out = op1 & op2;
-        //MISC-MEM Instructions
-        FENCE   :
-        FENCE_I :
-        //SYSTEM Instructions
-        ECALL   :
-        EBREAK  :
-        CSRRW   :
-        CSRRS   :
-        CSRRC   :
-        CSRRWI  :
-        CSRRSI  :
-        CSRRCI  :
-endmodule: alu
+    always_comb begin
+        case(instruction)
+            LUI     : 
+            AUIPC   :
+            JAL     :
+            JALR    :
+            //BRANCH Instructions
+            BEQ     :
+            BNE     :
+            BLT     :
+            BGE     :
+            BLTU    :
+            BGEU    :
+            //LOAD Instructions
+            LB      :
+            LH      :
+            LW      :
+            LBU     :
+            LHU     :
+            //STORE Instructions
+            SB      : 
+            SH      : 
+            SW      : 
+            //OP-IMM Instructions
+            ADDI    : 
+            SLTI    : 
+            SLTIU   : 
+            XORI    : 
+            ORI     : 
+            ANDI    : 
+            SLLI    : 
+            SRLI    : 
+            SRAI    : 
+            //OP Instructions
+            ADD     : op_out = 32'(op1 + op2);
+            SUB, SUBI: op_out = 32'(op1 - op2);
+            SLL     : op_out = op1 << op2; 
+            SLT     : op_out = (signed'(op1) < signed'(op2) ? 32'b1 : 32'b0);
+            SLTU    : op_out = (op1 < op2 ? 32'b1 : 32'b0);
+            XOR     : op_out = op1 ^ op2;
+            SRL     : op_out = op1 >> op2;
+            SRA     : op_out = op1 >>> op2;
+            OR      : op_out = op1 | op2;
+            AND     : op_out = op1 & op2;
+            //MISC-MEM Instructions
+            FENCE   :
+            FENCE_I :
+            //SYSTEM Instructions
+            ECALL   :
+            EBREAK  :
+            CSRRW   :
+            CSRRS   :
+            CSRRC   :
+            CSRRWI  :
+            CSRRSI  :
+            CSRRCI  :
+    end
+endmodule: Alu
