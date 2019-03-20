92df289d28a2f799804e798179ede826aa7f9f3a
NickSica <njs82@drexel.edu>
2019-03-20 17:29:30 -0400

    Changes

--- Top.sv
+++ Top.sv
@@ -8,16 +8,29 @@ interface registers(input logic clk);
     logic [31:0] pc;
 endinterface: registers
 */
-interface RAM_CPU();
-    logic[31:0] placeholder;
-    modport src(
+interface wishbone();
+    // Master to Slave
+    logic o_cyc; // 1 to start read or write
+    logic o_stb; // 1 to start read or write
+    logic o_we; // 0 for read, 1 for write
+    logic[2:0] o_addr; // Address of slave
+    logic o_data; // Data from master to slave
+    logic o_sel;
+
+    // Slave to Master
+    logic i_stall; // 0 to start read or write
+    logic i_ack; // Master must accept at any time after a transaction is initiated
+    logic i_data; // Data returned from slave to master
+    logic i_err;
+
+    modport master(
         input placeholder
     );
 
-    modport sink(
+    modport slave(
         output placeholder
     );
-endinterface: RAM_CPU
+endinterface: wishbone
 
 interface flags();
     logic[31:0] instr;
@@ -40,6 +53,7 @@ endinterface: flags;
 
 module Top(input clk);
     logic[7:0] bootcode[0:255];
+    wishbone wb();
 
     initial begin
         logic boot_complete = 1'b0;
@@ -48,6 +62,24 @@ module Top(input clk);
         end
     end
 
+    always_comb begin
+        case(wb.o_addr)
+            3'b000: begin
+                if(wb.o_stb && (!wb.i_stall)) begin
+                    if(!wb.o_we) begin
+                        Ram ram(.clk(clk), );
+                    end else begin
+                        Ram ram(.clk(clk), );
+                    end
+                end
+            end
+
+            3'b001: begin
+
+            end
+
+        endcase
+    end
 
     CPU cpu(.clk(clk), .fcpu(flags.cpu));
     RAM ram(.clk(clk), .fram(flags.ram));
